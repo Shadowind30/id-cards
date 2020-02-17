e22e20eade3701deb9c70831c1c14b3bd6cd5e1d
Shadowind30 <shadowind30@gmail.com>
2020-02-17 01:27:31 -0400

    Updates

---
+++ precache-manifest.76b06730d7ab48ab907ca90343dafd2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f19a8d71f60a67f341b5b1bf48f5861",
+    "url": "/id-cards/index.html"
+  },
+  {
+    "revision": "91e1070d26b43bb1605f",
+    "url": "/id-cards/static/js/2.d9b469e9.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/id-cards/static/js/2.d9b469e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd42b99570cd8151c8fb",
+    "url": "/id-cards/static/js/main.a7660001.chunk.js"
+  },
+  {
+    "revision": "46611566e5c85f5fe06f",
+    "url": "/id-cards/static/js/runtime-main.771250fd.js"
+  }
+]);
